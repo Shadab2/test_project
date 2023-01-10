8a2a59b85e161d38cb09015c9040676fbdb7c53c
Shaad <ssidique610@gmail.com>
2023-01-10 11:32:12 +0530

    add persistance

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import { Route, Routes } from "react-router-dom";
 import "./App.css";
 import Navbar from "./components/Navbar/Navbar";
+import Error from "./pages/Error/Error";
+import History from "./pages/History/History";
 import Home from "./pages/Home/Home";
 import Videos from "./pages/Videos/Videos";
 
@@ -11,6 +13,8 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="videos" element={<Videos />} />
+        <Route path="history" element={<History />} />
+        <Route path="*" element={<Error />} />
       </Routes>
     </div>
   );
