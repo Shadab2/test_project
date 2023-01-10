f4fac27a936610c042f5019a765f120236f62971
Shaad <ssidique610@gmail.com>
2023-01-10 13:42:18 +0530

    multiple-select and history api

--- src/pages/History/History.jsx
+++ src/pages/History/History.jsx
@@ -1,6 +1,10 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { selectHistory } from "../../redux/video/videoSlice";
 
 function History() {
+  const history = useSelector(selectHistory);
+  console.log(history);
   return <div>History</div>;
 }
 
